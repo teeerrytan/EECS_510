1a7b34281df5ca375e66567fcc4a1e95bb3e480e
Terry Tan <tmy1995hflc@gmail.com>
2019-05-11 17:44:45 -0500

    update stopwords

--- data.py
+++ data.py
@@ -5,12 +5,15 @@ import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from nltk.tokenize import TweetTokenizer
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 
 file = open('IMDB Dataset.csv')
 reader = csv.reader(file)
 rows = [row for row in reader]
 sentDetector = nltk.data.load('tokenizers/punkt/english.pickle')
+stop_words = set(stopwords.words('english'))
+
 
 positive = []
 negative = []
@@ -22,34 +25,46 @@ for i in rows:
         negative.append(i[0])
 pass
 
+
+def convert(s):
+    new = ""
+    for x in s:
+        new += x + " "
+    return new
+
+
 def Cleaner(List):
     newList = []
     cnt = 0
     for i in List:
         #cnt += 1
-        #print(cnt)
+        # print(cnt)
         # sentences = sentDetector.tokenize(i.get_text())
 
         # for s in sentences:
         i = i.lower()
-        cleaned = re.sub("[^a-zA-Z0-9 ]", "", i)
-        newList.append(cleaned)
+        cleaned = re.sub("[^a-zA-Z ]", "", i).split()
+        filtered_cleaned = [
+            word for word in cleaned if not word in stop_words]
+        str = convert(filtered_cleaned)
+        newList.append(str)
 
     return newList
+
 
 cleanedpositive = Cleaner(positive)
 cleanednegative = Cleaner(negative)
 pass
 
 
-fileObject = open('cleanedpositive.txt', 'w')
+fileObject = open('filteredpositive.txt', 'w')
 for i in cleanedpositive:
-	fileObject.write(i)
-	fileObject.write('\n')
+    fileObject.write(i)
+    fileObject.write('\n')
 fileObject.close()
 
-fileObject = open('cleanednegative.txt', 'w')
+fileObject = open('filterednegative.txt', 'w')
 for i in cleanednegative:
-	fileObject.write(i)
-	fileObject.write('\n')
+    fileObject.write(i)
+    fileObject.write('\n')
 fileObject.close()
--- student_code.py
+++ student_code.py
@@ -1,22 +1,23 @@
 import math
 import re
 
+
 class Bayes_Classifier:
 
     def __init__(self):
         pass
 
-
     def train(self, lines):
 
-        self.stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+        self.stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below',
+                           'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
         word_dict_5 = {}
         word_dict_1 = {}
         self.word_dict = {}
-        self.count1 = 0;
-        self.count5 = 0;
-        self.c1 = 0;
-        self.c5 = 0;
+        self.count1 = 0
+        self.count5 = 0
+        self.c1 = 0
+        self.c5 = 0
         for i in lines:
 
             res = i.split("|")
@@ -25,8 +26,6 @@ class Bayes_Classifier:
             sentence = self.punc(sentence)
             score = res[0]
             words = sentence.split(" ")
-
-
 
             if score == '5':
                 self.c5 += 1
@@ -55,12 +54,11 @@ class Bayes_Classifier:
         self.keys_1 = word_dict_1.keys()
         self.keys_5 = word_dict_5.keys()
 
-        w1= len(self.word_dict)
+        w1 = len(self.word_dict)
         w2 = len(word_dict_1)
-        w3= len(word_dict_5)
+        w3 = len(word_dict_5)
         self.featureset = {}
         for key in self.keys_1:
-
 
             p = (word_dict_1[key] + 1) / (len(self.word_dict) + self.count1)
             self.featureset[key, 1] = p
@@ -70,12 +68,6 @@ class Bayes_Classifier:
             self.featureset[key, 5] = p
 
         pass
-
-
-
-
-
-
 
     def classify(self, lines):
 
@@ -90,15 +82,15 @@ class Bayes_Classifier:
             p5 = self.c5 / (self.c1 + self.c5)
             p1 = self.c1 / (self.c1 + self.c5)
 
-
-
             for word in words:
 
                 word = word.lower()
                 if word not in self.keys_5:
-                    self.featureset[word, 5] = 1 / (len(self.word_dict) + self.count5)
+                    self.featureset[word, 5] = 1 / \
+                        (len(self.word_dict) + self.count5)
                 if word not in self.keys_1:
-                    self.featureset[word, 1] = 1 / (len(self.word_dict) + self.count1)
+                    self.featureset[word, 1] = 1 / \
+                        (len(self.word_dict) + self.count1)
 
                 if self.stop_words.count(word) == 1 or len(word) <= 2:
                     continue
@@ -109,8 +101,6 @@ class Bayes_Classifier:
                 prediction.append("5")
             else:
                 prediction.append("1")
-
-
 
         return prediction
 
